850fe367abefdffd2e3b24000a7607eaf4da19bf
John Koster <kosterjo@umich.edu>
2017-06-22 22:21:22 -0400

    added private members to the hashtable

--- hashtable.h
+++ hashtable.h
@@ -1,3 +1,4 @@
+#include <list>
 #include <string>
 
 using namespace std;
@@ -27,4 +28,14 @@ public:
   void shrink();
 
 private:
+
+  // the table, an array of lists - or buckets
+  list<int> table[];
+
+  // number of entries and buckets
+  int entries, buckets;
+
+  // minimum and maximum loads
+  double max = 0.75;
+  double min = 0.25;
 }
